b098bdb7ffa35cb88bbe87ca2600c886bb388fcf
Salir <lotharekon@gmail.com>
2015-02-21 12:58:01 +0100

    dodanie wyświetlania pozostałej energii wyspoażenia

--- EquipmentUI.cpp
+++ EquipmentUI.cpp
@@ -64,20 +64,58 @@ void EquipmentUI::reloadImages(Equipment *helmet, Equipment *chestplate, Equipme
 
 void EquipmentUI::display()
 {
-    if(selectedHelmet->isActive())al_draw_filled_rectangle(helmetCoords->X-5, helmetCoords->Y-5, helmetCoords->X + 105, helmetCoords->Y + 105, al_map_rgb(255,0,0));
+    int energy =100 * selectedHelmet->getCurrentEnergy() / selectedHelmet->getMaxEnergy();
+    if(selectedHelmet->isActive())al_draw_filled_rectangle(
+            helmetCoords->X-5 - Variables::offsetX, 
+            helmetCoords->Y-5 - Variables::offsetY, 
+            helmetCoords->X + 105 - Variables::offsetX, 
+            helmetCoords->Y + 105 - Variables::offsetY, 
+            al_map_rgb(255,0,0));
     helmetImage->display(helmetCoords);
-    if(selectedChestplate->isActive())al_draw_filled_rectangle(chestplateCoords->X-5, chestplateCoords->Y-5, chestplateCoords->X + 105, chestplateCoords->Y + 105, al_map_rgb(255,0,0));
+    al_draw_line(
+            helmetCoords->X - 5 - Variables::offsetX,
+            helmetCoords->Y + 105 - Variables::offsetY,
+            helmetCoords->X + 5 + energy - Variables::offsetX,
+            helmetCoords->Y + 105 - Variables::offsetY,
+            al_map_rgb(70,70,255), 4);
+    
+    energy =100 * selectedChestplate->getCurrentEnergy() / selectedChestplate->getMaxEnergy();
+    if(selectedChestplate->isActive())al_draw_filled_rectangle(
+            chestplateCoords->X-5 - Variables::offsetX, 
+            chestplateCoords->Y-5 - Variables::offsetY, 
+            chestplateCoords->X + 105 - Variables::offsetX, 
+            chestplateCoords->Y + 105 - Variables::offsetY, 
+            al_map_rgb(255,0,0));
     chestplateImage->display(chestplateCoords);
-    if(selectedGreaves->isActive())al_draw_filled_rectangle(greavesCoords->X-5, greavesCoords->Y-5, greavesCoords->X + 105, greavesCoords->Y + 105, al_map_rgb(255,0,0));
+    al_draw_line(
+            chestplateCoords->X - 5 - Variables::offsetX,
+            chestplateCoords->Y + 105 - Variables::offsetY,
+            chestplateCoords->X + 5 + energy - Variables::offsetX,
+            chestplateCoords->Y + 105 - Variables::offsetY,
+            al_map_rgb(70,70,255), 4);
+    
+    energy =100 * selectedGreaves->getCurrentEnergy() / selectedGreaves->getMaxEnergy();
+    if(selectedGreaves->isActive())al_draw_filled_rectangle(
+            greavesCoords->X-5 - Variables::offsetX, 
+            greavesCoords->Y-5 - Variables::offsetY, 
+            greavesCoords->X + 105 - Variables::offsetX, 
+            greavesCoords->Y + 105 - Variables::offsetY, 
+            al_map_rgb(255,0,0));
     greavesImage->display(greavesCoords);
+    al_draw_line(
+            greavesCoords->X - 5 - Variables::offsetX,
+            greavesCoords->Y + 105 - Variables::offsetY,
+            greavesCoords->X + 5 + energy - Variables::offsetX,
+            greavesCoords->Y + 105 - Variables::offsetY,
+            al_map_rgb(70,70,255), 4);
 }
 
 void EquipmentUI::update()
 {
     helmetCoords->X = 150 + Variables::offsetX;
-    helmetCoords->Y = Variables::RES_HEIGHT - 110 + Variables::offsetY;
+    helmetCoords->Y = Variables::RES_HEIGHT - 115 + Variables::offsetY;
     chestplateCoords->X = 275 + Variables::offsetX;
-    chestplateCoords->Y = Variables::RES_HEIGHT - 110 + Variables::offsetY;
+    chestplateCoords->Y = Variables::RES_HEIGHT - 115 + Variables::offsetY;
     greavesCoords->X = 400 + Variables::offsetX;
-    greavesCoords->Y = Variables::RES_HEIGHT - 110 + Variables::offsetY;
+    greavesCoords->Y = Variables::RES_HEIGHT - 115 + Variables::offsetY;
 }
